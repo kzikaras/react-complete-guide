c8bb8cc8dc92d3e41f0734993d7272d59ad971c0
kzikaras <kurtzikaras@gmail.com>
2023-03-15 21:50:15 -0400

    video 66 conditional content

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -8,13 +8,33 @@ function Expenses(props) {
     const [enteredFilter, setEnteredFilter] = useState('2020');
     const updateFilter = (filter) => {
         console.log(filter);
+        props.expenses.forEach(expense => {
+            console.log(expense.date.getFullYear());
+        });
         setEnteredFilter(filter);
+    }
+    let filteredExpenses = props.expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === enteredFilter;
+    });
+    let expensesContent = <p>No Expenses Found.</p>
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key={expense.id} 
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+            />
+        ))
     }
     return (
         <div>
             <Card className='expenses'>
-            <ExpensesFilter selected={ enteredFilter } fetchFilter= { updateFilter }/>
-            { props.expenses.map((expense) => (<ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)) }
+            <ExpensesFilter 
+                selected={ enteredFilter } 
+                fetchFilter= { updateFilter }
+            />
+            { expensesContent }
             </Card>
         </div>
     );
